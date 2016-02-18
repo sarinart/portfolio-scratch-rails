bf6fcb1983a01de40ca573016f0d558b67fba9b6
sarinart <sarinart@gmail.com>
2016-02-19 01:21:06 +1100

    redirect to index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
         @post = Post.new(post_params)
         @post.save
         
-        redirect_to @post
+        redirect_to posts_path
     end
     
     private

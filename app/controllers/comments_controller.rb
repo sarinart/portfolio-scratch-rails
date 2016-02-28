31b802b03440163da2babc134202ece7baeec89b
sarinart <sarinart@gmail.com>
2016-02-28 23:12:19 +1100

    delete post route fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,13 @@ class CommentsController < ApplicationController
         redirect_to post_path(@comment.post)
     end
     
+    def destroy
+        @post = Post.find params[:post_id]
+        @comment = @post.comments.find params[:id]
+        @comment.destroy
+        redirect_to post_path(@post)
+    end
+    
     private
         def comment_params
             params.require(:comment).permit(:author_name, :body)

6b23042217fa49760c1cb2e276d287e87d181af4
sarinart <sarinart@gmail.com>
2016-03-19 21:39:09 -0400

    about and contact routes added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
   def welcome
   end
+  
   def portfolio
   end
+  
   def index
         @posts = Post.all
   end
+  
+  def about
+  end
+  
+  def contact
+  end
+  
 end
